4d471d839331697628849266016e8bb07d114a0b
bellbezel <bellbezel@gmail.com>
2018-04-10 10:15:51 +0700

    Change code of console-weather_test.go

--- console-weather_test.go
+++ console-weather_test.go
@@ -2,95 +2,6 @@ package weather
 
 import "testing"
 
-func Test_toDigital_0(t *testing.T) {
-	expectedDigital := " _ \n" +
-		"| |\n" +
-		"|_|"
-	digital := toDigital(0)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_1(t *testing.T) {
-	expectedDigital := "   \n" +
-		"  |\n" +
-		"  |"
-	digital := toDigital(1)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_2(t *testing.T) {
-	expectedDigital := " _ \n" +
-		" _|\n" +
-		"|_ "
-	digital := toDigital(2)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_3(t *testing.T) {
-	expectedDigital := " _ \n" +
-		" _|\n" +
-		" _|"
-	digital := toDigital(3)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_4(t *testing.T) {
-	expectedDigital := "   \n" +
-		"|_|\n" +
-		"  |"
-	digital := toDigital(4)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_5(t *testing.T) {
-	expectedDigital := " _ \n" +
-		"|_ \n" +
-		" _|"
-	digital := toDigital(5)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_6(t *testing.T) {
-	expectedDigital := " _ \n" +
-		"|_ \n" +
-		"|_|"
-	digital := toDigital(6)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
-func Test_toDigital_7(t *testing.T) {
-	expectedDigital := " _ \n" +
-		"  |\n" +
-		"  |"
-	digital := toDigital(7)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-func Test_toDigital_8(t *testing.T) {
-	expectedDigital := " _ \n" +
-		"|_|\n" +
-		"|_|"
-	digital := toDigital(8)
-	if digital != expectedDigital {
-		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
-	}
-}
-
 func Test_toDigital_9(t *testing.T) {
 	expectedDigital := " _ \n" +
 		"|_|\n" +
@@ -135,6 +46,16 @@ func Test_toDigital_89(t *testing.T) {
 		"|_||_|\n" +
 		"|_| _|"
 	digital := toDigital(89)
+	if digital != expectedDigital {
+		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
+	}
+}
+
+func Test_toDigital_889(t *testing.T) {
+	expectedDigital := " _  _  _ \n" +
+		"|_||_||_|\n" +
+		"|_||_| _|"
+	digital := toDigital(889)
 	if digital != expectedDigital {
 		t.Error("Expected \n" + expectedDigital + " But actual is " + digital)
 	}
